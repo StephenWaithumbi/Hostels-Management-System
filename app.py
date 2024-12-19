3bb0fd28ecda1d5998cf6392f6083aa4c5022887
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-12-19 06:13:16 +0300

    creates the function to create a hostel

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from models import Hostel, Student, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///hostel_database.db'
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine('sqlite:///hostel_database.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -15,3 +13,13 @@ def init_db():
     #iniializes the db
     Base.metadata.create_all(engine)
     print("Hostel Database initialized...")
+
+def create_hostel():
+    #create a new hostel in the data base
+    name = input("Enter hostel name: >> ")
+    capacity = int(input("Enter the number rooms >> "))
+    hostel = Hostel(name=name, capacity=capacity)
+    session.add(hostel)
+    session.commit()
+    print(f"Hostel '{name}' created successfully")
+
